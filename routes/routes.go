6eb07528036f8053c8ebd0a060b3bd3d6ff3eae6
ArifulProtik <mdarifulislam.protik@gmail.com>
2020-04-13 12:41:20 +0600

    ⚡️ Query Param Added,Returning All Result in Slice

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,7 @@ func RegisterRouter(r *mux.Router) {
 	apiv1 := r.PathPrefix("/api/v1").Subrouter()
 	// Publicly Accesseble Endpoint goes here
 	apiv1.Handle("/", controller.BasicMiddleWare(controller.NotImplementedHandler))
-	apiv1.Handle("/search/{text}", controller.BasicMiddleWare(controller.SearchandGet)).Methods("GET")
-	apiv1.Handle("/posts/", controller.BasicMiddleWare(controller.Allposts)).Methods("GET")
-	apiv1.Handle("/post/{id}", controller.BasicMiddleWare(controller.Getpost))
+	apiv1.Handle("/search", controller.BasicMiddleWare(controller.SearchandGet)).Queries("q", "{q}").Methods("GET")
+	apiv1.Handle("/posts", controller.BasicMiddleWare(controller.Allposts)).Methods("GET")
+	apiv1.Handle("/post/{id}", controller.BasicMiddleWare(controller.Getpost)).Methods("GET")
 }
